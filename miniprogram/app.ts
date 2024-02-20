2329b937ad3f40f9bd55aa47cc4858a0d7e321ac
zhangone <zhangone233@126.com>
2024-02-20 19:47:54 +0800

    feat: stash

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -8,15 +8,28 @@ App<IAppOption>({
     wx.setStorageSync('logs', logs)
 
     // 登录
+    this.userLogin();
+  },
+
+  // 用户登录
+  userLogin() {
     wx.login({
       success: res => {
         console.log(res.code)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+
+        // wx.request({
+        //   url: 'http://127.0.0.1:4000/api/user/login',
+        //   method: 'POST',
+        //   data: {
+        //     code: res.code
+        //   },
+        //   success: (res) => {
+        //     console.log(res, 'res');
+
+        //   }
+        // })
       },
     })
-  },
-
-  userLogin() {
-
   }
 })
